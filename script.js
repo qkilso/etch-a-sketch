a4aba62cc2e8c0ddb1f1a0463ca7df8d5a1c2bf9
qkilso <thiennguyen1211@icloud.com>
2023-11-18 10:08:31 -0600

    added createPixel function

--- script.js
+++ script.js
@@ -6,4 +6,15 @@ function setCanvas(pixels){
     canvas.style.cssText =`background-color: blue; display: grid; grid-template-rows: repeat(${pixels}, 1fr); grid-template-columns: repeat(${pixels}, 1fr);`;
     return canvas;
 }
+
+function createPixel(parent, pixels){
+    totalPixels = pixels * pixels;
+    for(let i = 0; i < totalPixels; i++){
+    const pixel = document.createElement('div');
+    pixel.style.background = 'white';
+    pixel.style.border = '1px black solid';
+    parent.appendChild(pixel);
+    }
+}
+
 });
